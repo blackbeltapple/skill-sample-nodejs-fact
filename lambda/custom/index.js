d38062dc73c59cb42a27e64bae3f5babd8c9d8aa
Amanda Manekshaw <amanda.manekshaw@deepskybluesolutions.com>
2018-02-04 20:19:20 +0000

    Personalised for the manekshaws

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -29,19 +29,15 @@ const STOP_MESSAGE = 'Goodbye!';
 //TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/lambda/data
 //=========================================================================================================================================
 const data = [
-    'A year on Mercury is just 88 days long.',
-    'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-    'Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.',
-    'On Mars, the Sun appears about half the size as it does on Earth.',
-    'Earth is the only planet not named after a god.',
-    'Jupiter has the shortest day of all the planets.',
-    'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-    'The Sun contains 99.86% of the mass in the Solar System.',
-    'The Sun is an almost perfect sphere.',
-    'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-    'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-    'The temperature inside the Sun can reach 15 million degrees Celsius.',
-    'The Moon is moving approximately 3.8 cm away from our planet every year.'
+    'Sam is a berk and he has some fancy trainers.',
+    'The banana brothers are sat on the kitchen island.',
+    'Sam is wearing a Murdock sweatshirt.',
+    'I want to have tabasco with something but I don\'t know what to have it with?',
+    'I think an episode of Adventue Time has opened my third eye.',
+    'The Manekshaws rock!',
+    'Amanda\'s favourite film is blade runner',
+    'Jimmy wants to do a muscle up!',
+    'Sam is gettinhg really good at the guitar and should join or start band.'
 ];
 
 //=========================================================================================================================================
